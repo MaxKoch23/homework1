3ffb4e048ebdae7ba0ab4abdd77fcb319194bf85
Maxim <129218612+MaxKoch23@users.noreply.github.com>
2024-12-08 03:15:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+
+# 1st program
+print (9 ** 0.5 * 5)
+
+# 2nd program
+print (9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print (2 * 2 + 2 == 2 * (2 + 2))
+
+# 4th program
+print (int (float ('123.456') * 10) % 10 )
+
+# 5th program 1234 & 5678
+print ((1234 // 10 % 100) + (5678 // 10 % 100))
+
+# 6th program
+print (int(13.42) == 42.13 * 100 % 100 or int(42.13) == 13.42 * 100 % 100)
